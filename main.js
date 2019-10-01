d3601e39e4cdd4600b5474629d05d750e264efae
wingo66888 <wingo66888@gmail.com>
2019-10-01 13:26:58 +0800

    111

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+//--回到頂端
 $(document).ready(function () {
-    $('back-to-top').click(function (e) { 
-       $('html,body').animate({scrollTop:0}); 
+    $('back-to-top').click(function (e) {
+        $('html,body').animate({ scrollTop: 0 });
+    });
+
+    //--連結捲動到標題
+    $('.nav-link').click(function (e) {
+        var link = $(this).attr('href');
+        console.log(link);
     });
 });
